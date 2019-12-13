17076c00613b5914167c05d4cc381eb8a232dffe
marioxcolomar <marioxcolomar@gmail.com>
2019-12-13 13:41:40 +0100

    Add morgan and simplify db connection with URI.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 require('dotenv').config()
+const morgan = require('morgan')
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -12,16 +13,11 @@ const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : 'smart-brain'
-  }
+  connection: process.env.POSTGRES_URI
 });
 
 const app = express();
-
+app.use(morgan('combined'))
 app.use(cors())
 app.use(bodyParser.json());
 
